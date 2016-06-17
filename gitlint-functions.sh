3571b647a5cc984d0b67abf6f98e036c0f3a7bcb
Scott Lipcon <scott.lipcon@focusedsupport.com>
2016-06-17 11:02:56 -0400

    updates to tests, final tweaks
    updates to tests, tweaks to make it work for our environment

--- gitlint-functions.sh
+++ gitlint-functions.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# 
-
 isFeatureBranch()
 {
     refname="$1"
 
+    # if its a tag, its not a feature branch
+    if [[ $refname == refs/tags/* ]]; then
+        echo false
+        return
+    fi
+
     shortname=$($GIT rev-parse --symbolic --abbrev-ref $refname 2>/dev/null)
 
     if [ $? != 0 ]; then
-        echo false
-        return
+        # ok so refname isn't found.... but it could be a new branch:
+
+        if [[ $refname == refs/heads/* ]]; then
+            shortname=${refname#refs/heads/}  # pull off the refs/heads bit
+        else
+            echo false
+            return
+        fi
     fi
 
     case "$shortname" in
@@ -22,7 +32,6 @@ isFeatureBranch()
             ;;
     esac
     echo false
-
 }
 
 
@@ -45,6 +54,7 @@ cleanupGitlintCfg()
     rm -f $TMPFILE
 }
 
+
 buildJiraRegexp()
 {
     HOOKJIRAPROJECT=$($GIT config hook.jira-project)
@@ -52,6 +62,74 @@ buildJiraRegexp()
         return
     fi
 
-    echo "\b$HOOKJIRAPROJECT\b-\d+\b"
+    echo "-c T7.regex=\b$HOOKJIRAPROJECT\b-\d+\b"
 }
 
+validate_ref()
+{
+    # borrowed and adapted from https://github.com/andygrunwald/jitic
+
+    # Arguments
+    oldrev=$($GIT rev-parse $1)
+    newrev=$($GIT rev-parse $2)
+    refname="$3"
+
+    # $oldrev / $newrev are commit hashes (sha1) of git
+    # $refname is the full name of branch (refs/heads/*) or tag (refs/tags/*)
+    # $oldrev could be 0s which means creating $refname
+    # $newrev could be 0s which means deleting $refname
+
+    case "$refname" in
+        refs/heads/*)
+            # Pushing a branch delete...
+            if [ 0 -ne $(expr "$newrev" : "0*$") ]; then
+                COMMITS_TO_CHECK=""  # nothing to do
+
+            # Pushing a new branch
+            elif [ 0 -ne $(expr "$oldrev" : "0*$") ]; then
+                COMMITS_TO_CHECK=$($GIT rev-list $newrev --not --branches=*)
+
+            # Updating an existing branch
+            else
+                COMMITS_TO_CHECK=$($GIT rev-list $oldrev..$newrev)
+            fi
+
+            # If we push an new, but empty branch we can exit early.
+            # In this case there are no commits to check.
+            if [ -z "$COMMITS_TO_CHECK" ]; then
+                return
+            fi
+
+            CFG=$( getGitlintCfg $newrev )
+            REGEX=$( buildJiraRegexp )
+
+            # Get all commits, loop over and check if the comments are valid
+            while read REVISION ; do
+                $GIT log --pretty=format:"%B" -n 1 $REVISION | $GITLINT -vvv -C $CFG $REGEX
+                if [ $? != 0 ]; then
+                    FAIL=1
+                    echo >&2 "... in revision $REVISION"
+                fi
+            done <<< "$COMMITS_TO_CHECK"
+
+            cleanupGitlintCfg
+
+            return
+            ;;
+        refs/tags/*)
+            # Support for tags (new / delete) needs to be done.
+            # Things we need to check:
+            #   * Get all commits from the new tag that are NOT checked yet
+            #     (checked means by jitic). Something like commits which are
+            #     not pushed yet, but the tag was pushed.
+            # I think we don`t need to care about deleted branches yet.
+            ;;
+        *)
+            FAIL=1
+            echo >&2 ""
+            echo >&2 "*** pre-receive hook does not understand ref $refname in this repository. ***"
+            echo >&2 "*** Contact the repository administrator. ***"
+            echo >&2 ""
+            ;;
+    esac
+}
--- gitlint-tests.sh
+++ gitlint-tests.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-GIT=/usr/bin/git
+# Test program assumes git and gitlint are in the $PATH - but the functions
+# use these variables, since we don't really know the environment that will
+# be used from the hooks.
+
+GIT=git
+GITLINT=gitlint
 
 FUNCTIONS="gitlint-functions.sh"
 GITLINTCFG=$(pwd)/gitlint.cfg
 LOCAL_GITLINTCFG=.gitlint.cfg
-
 
 . $FUNCTIONS
 
@@ -16,48 +20,55 @@ if [ -d $TESTREPO ]; then
     exit 1;
 fi
 
-
 STARTDIR=$(pwd)
 mkdir $TESTREPO
 cd $TESTREPO
-$GIT init
-    
+$GIT init > /dev/null
 
 touch test-file
-git add test-file
-git commit -m 'initial commit'
-git branch develop
-git branch release-1.0-branch
-git branch feature/foo
-git branch user/bob
+$GIT add test-file > /dev/null
+$GIT commit -m 'initial commit' > /dev/null
+$GIT branch develop
+$GIT branch release-1.0-branch
+$GIT branch feature/foo
+$GIT branch user/bob
 
-FB=$(isFeatureBranch develop)
+FB=$(isFeatureBranch refs/heads/develop)
 if [ "false" != "$FB" ]; then
     echo "FAIL isFeatureBranch develop"
 fi
-FB=$(isFeatureBranch release-1.0-branch)
+FB=$(isFeatureBranch refs/heads/release-1.0-branch)
 if [ "false" != "$FB" ]; then
     echo "FAIL isFeatureBranch release-1.0-branch"
 fi
-FB=$(isFeatureBranch feature/foo)
+FB=$(isFeatureBranch refs/heads/feature/foo)
 if [ "true" != "$FB" ]; then
     echo "FAIL isFeatureBranch feature/foo"
 fi
-FB=$(isFeatureBranch user/bob)
+FB=$(isFeatureBranch refs/heads/user/bob)
 if [ "true" != "$FB" ]; then
     echo "FAIL isFeatureBranch user/bob"
 fi
-FB=$(isFeatureBranch unknown-branch)
+FB=$(isFeatureBranch refs/heads/unknown-branch)
 if [ "false" != "$FB" ]; then
     echo "FAIL isFeatureBranch unknown-branch"
 fi
-FB=$(isFeatureBranch feature/unknown)
-if [ "false" != "$FB" ]; then
+FB=$(isFeatureBranch refs/heads/feature/unknown)
+if [ "true" != "$FB" ]; then
     echo "FAIL isFeatureBranch feature/unknown"
 fi
+FB=$(isFeatureBranch refs/tags/v1.0.0)
+if [ "false" != "$FB" ]; then
+    echo "FAIL isFeatureBranch v1.0.0 tag"
+fi
 
+$GIT tag v0.9.0
+FB=$(isFeatureBranch refs/tags/v0.9.0)
+if [ "false" != "$FB" ]; then
+    echo "FAIL isFeatureBranch v0.9.0 tag"
+fi
 
-revision=$(git describe --always --long)
+revision=$($GIT describe --always --long)
 
 
 cfg=$( getGitlintCfg $revision )
@@ -72,12 +83,11 @@ fi
 
 cleanupGitlintCfg
 
-
 cp $GITLINTCFG ./.gitlint.cfg
-git add .
-git commit -m 'add gitlint.cfg'
+$GIT add . > /dev/null
+$GIT commit -m 'add gitlint.cfg' > /dev/null
 
-revision=$(git describe --always --long)
+revision=$($GIT describe --always --long)
 
 cfg=$( getGitlintCfg $revision )
 if [ "$cfg" == "$GITLINTCFG" ]; then
@@ -94,6 +104,39 @@ if [ -f "/tmp/gitlint.cfg.$$" ]; then
     echo "FAIL: cleanupGitlintCfg didn't clean temporary"
 fi
 
+
+cfg=$( getGitlintCfg $revision )
+regex=$( buildJiraRegexp )
+echo
+echo "Expect no gitlint fails:"
+cat $STARTDIR/test1.txt | $GITLINT -C $cfg $regex 
+cat $STARTDIR/test2.txt | $GITLINT -C $cfg $regex 
+cat $STARTDIR/test3.txt | $GITLINT -C $cfg $regex 
+
+$GIT config --local --add hook.jira-project PROJ
+regex=$( buildJiraRegexp )
+echo
+echo "Expect two gitlint fails:"
+cat $STARTDIR/test1.txt | $GITLINT -C $cfg $regex 
+cat $STARTDIR/test2.txt | $GITLINT -C $cfg $regex 
+cat $STARTDIR/test3.txt | $GITLINT -C $cfg $regex 
+
+cleanupGitlintCfg
+
+startrev=$($GIT describe --always --long)
+
+echo one >> test-file
+$GIT commit -a -F $STARTDIR/test1.txt > /dev/null
+echo two >> test-file
+$GIT commit -a -F $STARTDIR/test2.txt > /dev/null
+echo three >> test-file
+$GIT commit -a -F $STARTDIR/test3.txt > /dev/null
+
+endrev=$($GIT describe --always --long)
+
+echo
+echo "Running validate_ref: expect two gitlint fails:"
+validate_ref $startrev $endrev refs/heads/master
 
 cd $STARTDIR
 rm -rf $TESTREPO
